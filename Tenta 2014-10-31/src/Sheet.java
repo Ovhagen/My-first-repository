7f012414fdd42fd5e951e0af64c4945d5f324074
Pontus Ovhagen <ovhagen@gmail.com>
2015-10-03 11:52:27 +0200

    Tenta 2014-10-31

---
+++ Tenta 2014-10-31/src/Sheet.java
@@ -0,0 +1,21 @@
+
+public class Sheet extends Observable implements Environment {
+	private SlotFactory slotFactory = new SlotFactory();
+	private Map<String, Slot> map = new HashMap<String, Slot>();
+
+	public void clear(String address) {
+		// code omitted
+	}
+
+	public void set(String address, String string) {
+		Slot newSlot = slotFactory.build(string);
+		map.put(address, newSlot);
+		setChanged();
+		notifyObservers();
+	}
+
+	public String toString(String address) {
+		Slot slot = map.get(address);
+		return (slot == null) ? "" : slot.toString();
+	}
+}
